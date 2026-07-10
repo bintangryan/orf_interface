9da5698425fd6661f18fc66b86ebe2a1349edca5
BINTANG RYAN WARDANA <143849730+bintangryan@users.noreply.github.com>
2026-07-10 20:11:39 +0700

    Update uvicorn run configuration to use environment port

--- backend/main.py
+++ backend/main.py
@@ -175,5 +175,12 @@ def predict(data: JobInput):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        reload=False,
+    )
